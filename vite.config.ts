23220f127feb4fc0b202122887f7e3e50a1bf4df
NextZac <48414018+NextZac@users.noreply.github.com>
2023-12-19 13:55:39 +0200

    Removed some items

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { liveDesigner } from '@pinegrow/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [liveDesigner({tailwindcss: {
-    /* Please ensure that you update the filenames and paths to accurately match those used in your project. */
-    configPath: 'tailwind.config.js',
-    cssPath: '@/style.css',
-    // themePath: false, // Set to false so that Design Panel is not used
-    // restartOnConfigUpdate: true,
-    restartOnThemeUpdate: true,
-  },}),vue()],
+  plugins: [vue()],
 })
